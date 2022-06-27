6577c69f47570bcad5cdf1342196f99a52c9b99f
programmal <alexanderlukeroberts@gmail.com>
2022-06-27 19:00:57 -0400

    Commit #2 - Added an additional function to repeat the game for up to 5 rounds.

--- rps.js
+++ rps.js
@@ -2,33 +2,38 @@
 
 function computerPlay () {
     const cchoices = ["rock", "paper", "scissors"];
-    const cplay = Math.floor (Math.random() * cchoices.length);
+    cplay = Math.floor (Math.random() * cchoices.length);
     console.log ("Computer chooses " + cchoices[cplay] + ".");
 } 
 
 function playerSelection() {
     const pchoice = prompt("RPS: What is your choice?");
-    const actualchoice = pchoice.toLowerCase("");
-    }
+    actualchoice = pchoice.toLowerCase("");
+    console.log ("Player chooses " + actualchoice + ".");}
 
-function playGame(cplay, actualchoice){
-    
+function playRound(){
+
 playerSelection()
 computerPlay()
 
- if (actualchoice == "rock" && cplay == "scissors") {
+ if (actualchoice == "rock" && cplay == 2) {
         console.log ("Rock beats scissors, player win!")
-    } else if (actualchoice == "scissors" && cplay == "rock") {
+    } else if (actualchoice == "scissors" && cplay == 0) {
         console.log ("Rock beats scissors, computer win!")
-    } else if (actualchoice == "paper" && cplay == "rock") {
+    } else if (actualchoice == "paper" && cplay == 0) {
         console.log ("Paper beats rock, player win!")
-    } else if (actualchoice == "rock" && cplay == "paper") {
+    } else if (actualchoice == "rock" && cplay == 1) {
         console.log ("Paper beats rock, computer win!")
-    } else if (actualchoice == "scissors" && cplay == "paper") {
+    } else if (actualchoice == "scissors" && cplay == 1) {
         console.log ("Scissors beat paper, player win!")
-    } else if (actualchoice == "paper" && cplay == "scissors") {
+    } else if (actualchoice == "paper" && cplay == 2) {
         console.log ("Scissors beat paper, computer win!")
     } else {
-        console.log ("Something went wrong, try again.")
-    }
-}
+        console.log ("Tie!")
+    }}
+
+function playGame() {
+    for (let g = 0; g <5; g++){
+        playRound()}}
+
+playGame();
